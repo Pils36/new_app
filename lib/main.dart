f57eadb98f2dde1426c415269f67699082492b76
Adenuga Adebambo <adenugaadebambo41@gmail.com>
2020-12-11 12:31:36 +0100

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I am Rich'),
+          title: Text('Udemy Course - I am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
